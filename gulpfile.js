bfbf4aafdae5e07941d4750be0cd1670e14e804c
Nicolas Widart <n.widart@gmail.com>
2017-01-09 10:56:05 +0100

    Renaming AdminLTE to Adminlte.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
+var gulp = require("gulp");
+var shell = require('gulp-shell');
 var elixir = require('laravel-elixir');
+var themeInfo = require('./theme.json');
+
+var task = elixir.Task;
+elixir.extend("stylistPublish", function() {
+    new task("stylistPublish", function() {
+        return gulp.src("").pipe(shell("php ../../artisan stylist:publish " + themeInfo.name));
+    }).watch("**/*.less");
+});
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +21,23 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+elixir(function (mix) {
+
+    /**
+     * Compile less
+     */
+    mix
+        .less([
+            "asgard.less"
+        ], 'assets/css/asgard.css')
+        .stylistPublish();
+
+    /**
+     * Concat scripts
+     */
+    //mix.scripts([
+    //    "app.js"
+    //]);
+
+    mix.copy('resources/assets/js', 'assets/js');
 });
